6c0e17fdaf5e24c89701e71157642621c64fbfc7
Tomas Psota <to.psota@gmail.com>
2023-05-15 14:39:05 +0200

    feat: add style linters

--- hello/www/js/index.js
+++ hello/www/js/index.js
@@ -17,122 +17,120 @@
  * under the License.
  */
 
+/* global cordova, talsec */
+
 // Wait for the deviceready event before using any of Cordova's device APIs.
 // See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
 document.addEventListener('deviceready', onDeviceReady, false);
 
-function onDeviceReady() {
+function onDeviceReady () {
     // Cordova is now initialized. Have fun!
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+    console.log(`Running cordova-${cordova.platformId}@${cordova.version}`);
 
-    var checks = {
-        'privilegedAccess': 'Privileged Access',
-        'debug': 'Debug',
-        'simulator': 'Simulator',
-        'appIntegrity': 'App Integrity',
-        'unofficialStore': 'Unofficial Store',
-        'hooks': 'Hooks',
-        'device binding': 'Device binding'
+    const checks = {
+        privilegedAccess: 'Privileged Access',
+        debug: 'Debug',
+        simulator: 'Simulator',
+        appIntegrity: 'App Integrity',
+        unofficialStore: 'Unofficial Store',
+        hooks: 'Hooks',
+        deviceBinding: 'Device binding',
+        passcode: 'Passcode',
+        secureHardwareNotAvailable: 'Secure Hardware Not Available'
+    };
+
+    if (cordova.platformId === 'ios') {
+        checks.deviceID = 'Device ID';
     }
-    
-    if (cordova.platformId == 'ios') {
-        checks['deviceID'] = 'Device ID';
-        checks['missingSecureEnclave'] = 'Missing Secure Enclave';
-        checks['passcodeChange'] = 'Passcode Change';
-        checks['passcode'] = 'Passcode';
-    }
-    
+
     Object.entries(checks).forEach(([check, msg]) => {
-        const newElem = Object.assign(
-            document.createElement('div'),
-            { id: check,
-              innerHTML: '<p class="event received">' + msg + '</p>'});
-        document.getElementById('parent').appendChild(newElem)
+        const newElem = Object.assign(document.createElement('div'), {
+            id: check,
+            innerHTML: `<p class="event received">${msg}</p>`
+        });
+        document.getElementById('parent').appendChild(newElem);
         setDefaultThreatStyle(check);
-    })
-    
-    var config = {
-            androidConfig : {
-                packageName : "com.example.helloapp",
-                certificateHashes : ["your_signing_certificate_hash_base64"],
-	            // supportedAlternativeStores: ['storeOne', 'storeTwo'],
-            },
-            iosConfig : {
-                appBundleIds: "com.example.helloapp",
-                appTeamId: "your_team_ID",
-            },
-            watcherMail : "your_email_address@example.com",
-            isProd: true
-        };
-    
-    var threatListener = function(threatType) {
-            switch(threatType) {
-                case "privilegedAccess": // Android & iOS
-                    console.log('privilegedAccess');
-                    changeThreatStyle('privilegedAccess');
-                    break;
-                case "debug": // Android & iOS
-                    console.log('debug');
-                    changeThreatStyle('debug');
-                    break;
-                case "simulator": // Android & iOS
-                    console.log('simulator');
-                    changeThreatStyle('simulator');
-                    break;
-                case "appIntegrity": // Android & iOS
-                    console.log('appIntegrity');
-                    changeThreatStyle('appIntegrity');
-                    break;
-                case "unofficialStore": // Android & iOS
-                    console.log('unofficialStore');
-                    changeThreatStyle('unofficialStore');
-                    break;
-                case "hooks": // Android & iOS
-                    console.log('hooks');
-                    changeThreatStyle('hooks');
-                    break;
-                case "device binding": // Android & iOS
-                    console.log('device binding');
-                    changeThreatStyle('device binding');
-                    break;
-                case "deviceID": // iOS only
-                    console.log('deviceID');
-                    changeThreatStyle('deviceID');
-                    break;
-                case "missingSecureEnclave": // iOS only
-                    console.log('missingSecureEnclave');
-                    changeThreatStyle('missingSecureEnclave');
-                    break;
-                case "passcodeChange": // iOS only
-                    console.log('passcodeChange');
-                    changeThreatStyle('passcodeChange');
-                    break;
-                case "passcode": // iOS only
-                    console.log('passcode');
-                    changeThreatStyle('passcode');
-                    break;
-                default:
-                    console.log('Unknown threat type detected: ' + threatType);
-            }
-        };
-    
-    talsec.start(config, threatListener).then(() => {
-        console.log('Talsec initialized.');
-    }).catch((error) => {
-        console.log('Error during Talsec initialization: ', error);
     });
+
+    const config = {
+        androidConfig: {
+            packageName: 'com.example.helloapp',
+            certificateHashes: ['your_signing_certificate_hash_base64']
+            // supportedAlternativeStores: ['storeOne', 'storeTwo'],
+        },
+        iosConfig: {
+            appBundleIds: 'com.example.helloapp',
+            appTeamId: 'your_team_ID'
+        },
+        watcherMail: 'your_email_address@example.com',
+        isProd: true
+    };
+
+    const threatListener = function (threatType) {
+        switch (threatType) {
+        case 'privilegedAccess': // Android & iOS
+            console.log('privilegedAccess');
+            changeThreatStyle('privilegedAccess');
+            break;
+        case 'debug': // Android & iOS
+            console.log('debug');
+            changeThreatStyle('debug');
+            break;
+        case 'simulator': // Android & iOS
+            console.log('simulator');
+            changeThreatStyle('simulator');
+            break;
+        case 'appIntegrity': // Android & iOS
+            console.log('appIntegrity');
+            changeThreatStyle('appIntegrity');
+            break;
+        case 'unofficialStore': // Android & iOS
+            console.log('unofficialStore');
+            changeThreatStyle('unofficialStore');
+            break;
+        case 'hooks': // Android & iOS
+            console.log('hooks');
+            changeThreatStyle('hooks');
+            break;
+        case 'deviceBinding': // Android & iOS
+            console.log('deviceBinding');
+            changeThreatStyle('deviceBinding');
+            break;
+        case 'secureHardwareNotAvailable': // Android & iOS
+            console.log('secureHardwareNotAvailable');
+            changeThreatStyle('secureHardwareNotAvailable');
+            break;
+        case 'passcode': // Android & iOS
+            console.log('passcode');
+            changeThreatStyle('passcode');
+            break;
+        case 'deviceID': // iOS only
+            console.log('deviceID');
+            changeThreatStyle('deviceID');
+            break;
+        default:
+            console.log(`Unknown threat type detected: ${threatType}`);
+        }
+    };
+
+    talsec
+        .start(config, threatListener)
+        .then(() => {
+            console.log('Talsec initialized.');
+        })
+        .catch((error) => {
+            console.log('Error during Talsec initialization: ', error);
+        });
 }
 
-
-function changeThreatStyle(threatType) {
-    document.getElementById(threatType).style.backgroundColor = "red";
+function changeThreatStyle (threatType) {
+    document.getElementById(threatType).style.backgroundColor = 'red';
 }
 
-
-function setDefaultThreatStyle(threatType) {
-    document.getElementById(threatType).style.backgroundColor = "green";
-    document.getElementById(threatType).style.borderRadius = "4px";
-    document.getElementById(threatType).style.margin = "4px 10px";
-    document.getElementById(threatType).style.padding = "2px 0px";
-    document.getElementById(threatType).style.textAlign = "center";
+function setDefaultThreatStyle (threatType) {
+    document.getElementById(threatType).style.backgroundColor = 'green';
+    document.getElementById(threatType).style.borderRadius = '4px';
+    document.getElementById(threatType).style.margin = '4px 10px';
+    document.getElementById(threatType).style.padding = '2px 0px';
+    document.getElementById(threatType).style.textAlign = 'center';
 }
--- www/talsec.js
+++ www/talsec.js
@@ -1,22 +1,19 @@
-/*global cordova, module*/
+/* global cordova */
 
-module.exports = {
-    start: function (config, eventListener) {
-        return new Promise((resolve, reject) => {
-            cordova.exec(
-                (success) => {
-                    if (success != null && success == "started") {
-                        resolve();
-                    }
-                    else {
-                        eventListener(success);
-                    }
-                },
-                (error) => {
-                    reject(error);
-                },
-                "TalsecPlugin", "start", [config]
-            );
-        })
-    }
-};
+export function start (config, eventListener) {
+    return new Promise((resolve, reject) => {
+        cordova.exec(
+            (success) => {
+                if (success !== null && success === 'started') {
+                    resolve();
+                } else {
+                    eventListener(success);
+                }
+            },
+            (error) => {
+                reject(error);
+            },
+            'TalsecPlugin', 'start', [config]
+        );
+    });
+}
