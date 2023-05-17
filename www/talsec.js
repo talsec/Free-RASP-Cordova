2790d278b9215d96ecad1bc8ca92c695aa0ce329
Tomas Psota <to.psota@gmail.com>
2023-05-17 15:28:40 +0200

    feat: update module export

--- www/talsec.js
+++ www/talsec.js
@@ -1,19 +1,23 @@
-/* global cordova */
+/* global cordova, module */
 
-export function start (config, eventListener) {
-    return new Promise((resolve, reject) => {
-        cordova.exec(
-            (success) => {
-                if (success !== null && success === 'started') {
-                    resolve();
-                } else {
-                    eventListener(success);
-                }
-            },
-            (error) => {
-                reject(error);
-            },
-            'TalsecPlugin', 'start', [config]
-        );
-    });
-}
+module.exports = {
+    start: function (config, eventListener) {
+        return new Promise((resolve, reject) => {
+            cordova.exec(
+                (success) => {
+                    if (success != null && success === 'started') {
+                        resolve();
+                    } else {
+                        eventListener(success);
+                    }
+                },
+                (error) => {
+                    reject(error);
+                },
+                'TalsecPlugin',
+                'start',
+                [config]
+            );
+        });
+    }
+};
