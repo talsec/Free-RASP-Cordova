17123ca5fc01bf6a67d8aec02159a5d84dde5b3d
Tomas Psota <to.psota@gmail.com>
2023-11-20 18:07:57 +0100

    feat(iOS): rename callbacks

--- src/ios/TalsecPlugin.swift
+++ src/ios/TalsecPlugin.swift
@@ -2,38 +2,52 @@ import Foundation
 import TalsecRuntime
 
 @objc(TalsecPlugin) class TalsecPlugin : CDVPlugin {
+    public static var shared:TalsecPlugin?
+    let threatIdentifierList = (1...12).map { _ in Int.random(in: 100_000..<999_999_999) }
+    
+    override func pluginInitialize() {
+        TalsecContext.context.commandDelegate = self.commandDelegate
+        TalsecPlugin.shared = self
+    }
+    
+    /**
+     * Method to get the random identifiers of callbacks
+     */
+    @objc(getThreatIdentifiers:)
+    private func getThreatIdentifiers(command: CDVInvokedUrlCommand) -> Void {
+        let threatIdentifiers = SecurityThreat.allCases
+            .filter {
+                threat in threat.rawValue != "passcodeChange"
+            }
+            .map {
+                threat in threat.callbackIdentifier
+            }
+        TalsecContext.sendMessage(msg: threatIdentifiers, callbackId: command.callbackId, keepCallback: false)
+    }
+    
     @objc(start:)
     func start(command: CDVInvokedUrlCommand) {
-        SecurityThreatContainer.shared.callbackId = command.callbackId
-        SecurityThreatContainer.shared.commandDelegate = self.commandDelegate
+        TalsecContext.context.listenerCallbackId = command.callbackId
         
         guard let talsecConfig = command.arguments[0] as? NSDictionary else {
-            SecurityThreatCenter.sendError(msg: "Missing config parameter in Talsec Native Plugin")
+            TalsecContext.sendError(msg: "Missing config parameter in Talsec Native Plugin", callbackId: command.callbackId)
             return
         }
         do {
             try initializeTalsec(talsecConfig: talsecConfig)
         }
         catch let error as NSError {
-            SecurityThreatCenter.sendError(msg: error.localizedDescription)
+            TalsecContext.sendError(msg: error.localizedDescription, callbackId: command.callbackId)
             return
         }
-
-        DispatchQueue.main.async {
-            let pluginResult = CDVPluginResult(
-                status: CDVCommandStatus_OK,
-                messageAs: "started"
-            )
-
-            pluginResult?.setKeepCallbackAs(true)
-
-            SecurityThreatContainer.shared.commandDelegate!.send(
-                pluginResult,
-                callbackId: SecurityThreatContainer.shared.callbackId
-            )
-        }
+        TalsecContext.sendMessage(msg: "started", callbackId: TalsecContext.context.listenerCallbackId, keepCallback: true)
     }
-
+    
+    @objc(onInvalidCallback:)
+    private func onInvalidCallback(command: CDVInvokedUrlCommand) -> Void {
+        abort()
+    }
+    
     func initializeTalsec(talsecConfig: NSDictionary) throws {
         guard let iosConfig = talsecConfig["iosConfig"] as? NSDictionary else {
             throw NSError(domain: "Missing iosConfig parameter in Talsec Native Plugin", code: 1)
@@ -59,46 +73,93 @@ import TalsecRuntime
 extension SecurityThreatCenter: SecurityThreatHandler {
     public func threatDetected(_ securityThreat: TalsecRuntime.SecurityThreat) {
         // It is better to implement security reactions (e.g. killing the app) here.
-        SecurityThreatCenter.sendMessage(msg: securityThreat.rawValue)
-    }
-
-    static let threatEventMap: [String: String] = [
-        "missingSecureEnclave": "secureHardwareNotAvailable",
-        "device binding": "deviceBinding",
-    ]
-
-    static func sendMessage(msg: String) {
-        if (msg == "passcodeChange") {
+        
+        if (securityThreat.rawValue == "passcodeChange") {
             return
         }
-
-        let pluginResult = CDVPluginResult(
-            status: CDVCommandStatus_OK,
-            messageAs: threatEventMap[msg] ?? msg
-        )
-
-        pluginResult?.setKeepCallbackAs(true)
-
-        SecurityThreatContainer.shared.commandDelegate!.send(
-            pluginResult,
-            callbackId: SecurityThreatContainer.shared.callbackId
-        )
-    }
-
-    static func sendError(msg: String) {
-        let pluginResult = CDVPluginResult(
-            status: CDVCommandStatus_ERROR,
-            messageAs: msg
-        )
-
-        SecurityThreatContainer.shared.commandDelegate!.send(
-            pluginResult,
-            callbackId: SecurityThreatContainer.shared.callbackId
-        )
+        TalsecContext.sendMessage(msg: securityThreat.callbackIdentifier, callbackId: TalsecContext.context.listenerCallbackId)
     }
 }
 
-class SecurityThreatContainer : CDVPlugin {
-    static let shared = SecurityThreatContainer()
-    var callbackId = "0"
+class TalsecContext : CDVPlugin {
+    static let context = TalsecContext()
+    var listenerCallbackId = "0"
+
+    static func sendMessage(msg: Any, callbackId: String, keepCallback: Bool = true) {
+        // send the result to JavaScript on the main thread
+        DispatchQueue.main.async {
+            var pluginResult: CDVPluginResult?
+
+            if let intMsg = msg as? Int {
+                pluginResult = CDVPluginResult(
+                    status: CDVCommandStatus_OK,
+                    messageAs: intMsg
+                )
+            } else if let stringMsg = msg as? String {
+                pluginResult = CDVPluginResult(
+                    status: CDVCommandStatus_OK,
+                    messageAs: stringMsg
+                )
+            } else if let arrayMsg = msg as? [Any] {
+                pluginResult = CDVPluginResult(
+                    status: CDVCommandStatus_OK,
+                    messageAs: arrayMsg
+                )
+            }
+
+            pluginResult?.setKeepCallbackAs(keepCallback)
+            
+            context.commandDelegate?.send(
+                pluginResult,
+                callbackId: callbackId
+            )
+        }
+    }
+
+    static func sendError(msg: String, callbackId: String) {
+        DispatchQueue.main.async {
+            // send the result to JavaScript on the main thread
+            let pluginResult = CDVPluginResult(
+                status: CDVCommandStatus_ERROR,
+                messageAs: msg
+            )
+            
+            context.commandDelegate?.send(
+                pluginResult,
+                callbackId: callbackId
+            )
+        }
+    }
+}
+
+/// An extension to unify callback names with Cordova ones.
+extension SecurityThreat {
+    var callbackIdentifier: Int {
+        switch self {
+            case .signature:
+            return TalsecPlugin.shared!.threatIdentifierList[0]
+            case .jailbreak:
+                return TalsecPlugin.shared!.threatIdentifierList[1]
+            case .debugger:
+                return TalsecPlugin.shared!.threatIdentifierList[2]
+            case .runtimeManipulation:
+                return TalsecPlugin.shared!.threatIdentifierList[3]
+            case .passcode:
+                return TalsecPlugin.shared!.threatIdentifierList[4]
+            case .passcodeChange:
+                return TalsecPlugin.shared!.threatIdentifierList[5]
+            case .simulator:
+                return TalsecPlugin.shared!.threatIdentifierList[6]
+            case .missingSecureEnclave:
+                return TalsecPlugin.shared!.threatIdentifierList[7]
+            case .deviceChange:
+                return TalsecPlugin.shared!.threatIdentifierList[8]
+            case .deviceID:
+                return TalsecPlugin.shared!.threatIdentifierList[9]
+            case .unofficialStore:
+                return TalsecPlugin.shared!.threatIdentifierList[10]
+            @unknown default:
+                abort()
+        }
+    }
 }
