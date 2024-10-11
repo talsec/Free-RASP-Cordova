7288701b58c21d89e12fbda8b5e73c3e6099f686
Tomas Psota <to.psota@gmail.com>
2024-10-11 16:33:15 +0200

    feat(Android): add malware detection

--- src/android/TalsecThreatHandler.kt
+++ src/android/TalsecThreatHandler.kt
@@ -4,59 +4,63 @@ import com.aheaditec.talsec_security.security.api.SuspiciousAppInfo
 import com.aheaditec.talsec_security.security.api.ThreatListener
 
 internal object TalsecThreatHandler : ThreatListener.ThreatDetected, ThreatListener.DeviceState {
-    internal var listener: TalsecCordova? = null
+  internal var listener: TalsecCordova? = null
 
-    override fun onRootDetected() {
-        listener?.threatDetected(Threat.PrivilegedAccess)
-    }
+  override fun onRootDetected() {
+    listener?.threatDetected(Threat.PrivilegedAccess)
+  }
 
-    override fun onDebuggerDetected() {
-        listener?.threatDetected(Threat.Debug)
-    }
+  override fun onDebuggerDetected() {
+    listener?.threatDetected(Threat.Debug)
+  }
 
-    override fun onEmulatorDetected() {
-        listener?.threatDetected(Threat.Simulator)
-    }
+  override fun onEmulatorDetected() {
+    listener?.threatDetected(Threat.Simulator)
+  }
 
-    override fun onTamperDetected() {
-        listener?.threatDetected(Threat.AppIntegrity)
-    }
+  override fun onTamperDetected() {
+    listener?.threatDetected(Threat.AppIntegrity)
+  }
 
-    override fun onUntrustedInstallationSourceDetected() {
-        listener?.threatDetected(Threat.UnofficialStore)
-    }
+  override fun onUntrustedInstallationSourceDetected() {
+    listener?.threatDetected(Threat.UnofficialStore)
+  }
 
-    override fun onHookDetected() {
-        listener?.threatDetected(Threat.Hooks)
-    }
+  override fun onHookDetected() {
+    listener?.threatDetected(Threat.Hooks)
+  }
 
-    override fun onDeviceBindingDetected() {
-        listener?.threatDetected(Threat.DeviceBinding)
-    }
+  override fun onDeviceBindingDetected() {
+    listener?.threatDetected(Threat.DeviceBinding)
+  }
 
-    override fun onUnlockedDeviceDetected() {
-        listener?.threatDetected(Threat.Passcode)
-    }
+  override fun onUnlockedDeviceDetected() {
+    listener?.threatDetected(Threat.Passcode)
+  }
 
-    override fun onHardwareBackedKeystoreNotAvailableDetected() {
-        listener?.threatDetected(Threat.SecureHardwareNotAvailable)
-    }
+  override fun onHardwareBackedKeystoreNotAvailableDetected() {
+    listener?.threatDetected(Threat.SecureHardwareNotAvailable)
+  }
 
-    override fun onObfuscationIssuesDetected() {
-        listener?.threatDetected(Threat.ObfuscationIssues)
-    }
+  override fun onObfuscationIssuesDetected() {
+    listener?.threatDetected(Threat.ObfuscationIssues)
+  }
 
-    override fun onMalwareDetected(p0: MutableList<SuspiciousAppInfo>?) {}
+  override fun onMalwareDetected(suspiciousAppInfos: MutableList<SuspiciousAppInfo>?) {
+    listener?.malwareDetected(suspiciousAppInfos ?: mutableListOf())
+  }
 
-    override fun onDeveloperModeDetected() {
-       listener?.threatDetected(Threat.DevMode)
-    }
+  override fun onDeveloperModeDetected() {
+    listener?.threatDetected(Threat.DevMode)
+  }
 
-    override fun onSystemVPNDetected() {
-       listener?.threatDetected(Threat.SystemVPN)
-    }
+  override fun onSystemVPNDetected() {
+    listener?.threatDetected(Threat.SystemVPN)
+  }
 
-    internal interface TalsecCordova {
-        fun threatDetected(threatType: Threat)
-    }
+  internal interface TalsecCordova {
+    fun threatDetected(threatType: Threat)
+
+    fun malwareDetected(suspiciousApps: MutableList<SuspiciousAppInfo>)
+  }
 }
