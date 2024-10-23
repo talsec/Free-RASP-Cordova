fd031a83a9da8f1284ca3cf637197cfc25982179
Tomas Psota <to.psota@gmail.com>
2024-10-23 10:44:26 +0200

    refactor: removed nested conditions from gradle script

--- example/scripts/modifyGradle.js
+++ example/scripts/modifyGradle.js
@@ -11,56 +11,60 @@ module.exports = function (context) {
     'platforms/android/build.gradle',
   );
 
-  if (fs.existsSync(gradleFilePath)) {
-    let gradleFile = fs.readFileSync(gradleFilePath, 'utf8');
+  if (!fs.existsSync(gradleFilePath)) {
+    console.error('build.gradle file not found');
+    return;
+  }
 
-    // Define the custom plugins block
-    const pluginConfig = `
+  let gradleFile = fs.readFileSync(gradleFilePath, 'utf8');
+
+  // Check if the plugins block already exists
+  if (gradleFile.includes('plugins {')) {
+    console.log('Custom plugins block already exists in build.gradle');
+    return;
+  }
+
+  // Find the start of the 'buildscript' block
+  const buildscriptStart = gradleFile.indexOf('buildscript');
+  if (buildscriptStart === -1) {
+    console.error('buildscript block not found in build.gradle');
+    return;
+  }
+
+  // Find the end of the 'buildscript' block, accounting for nested braces
+  let openBraces = 0;
+  let closeBraces = 0;
+  let buildscriptEnd = -1;
+
+  for (let i = buildscriptStart; i < gradleFile.length; i++) {
+    if (gradleFile[i] === '{') openBraces++;
+    if (gradleFile[i] === '}') closeBraces++;
+
+    // When all opened braces are closed, we've reached the end
+    if (openBraces > 0 && openBraces === closeBraces) {
+      buildscriptEnd = i + 1;
+      break;
+    }
+  }
+
+  if (buildscriptEnd <= 0) {
+    console.error('Could not find the end of the buildscript block');
+    return;
+  }
+
+  // Insert the plugins block after the 'buildscript' section
+  // Define the custom plugins block
+  const pluginConfig = `
 
 // Custom plugin property
 plugins {
     id 'org.jetbrains.kotlin.plugin.serialization' version '1.7.10'
 }`;
 
-    // Check if the plugins block already exists
-    if (!gradleFile.includes('plugins {')) {
-      // Find the start of the 'buildscript' block
-      const buildscriptStart = gradleFile.indexOf('buildscript');
-      if (buildscriptStart > -1) {
-        // Find the end of the 'buildscript' block, accounting for nested braces
-        let openBraces = 0;
-        let closeBraces = 0;
-        let buildscriptEnd = -1;
-
-        for (let i = buildscriptStart; i < gradleFile.length; i++) {
-          if (gradleFile[i] === '{') openBraces++;
-          if (gradleFile[i] === '}') closeBraces++;
-
-          // When all opened braces are closed, we've reached the end
-          if (openBraces > 0 && openBraces === closeBraces) {
-            buildscriptEnd = i + 1;
-            break;
-          }
-        }
-
-        if (buildscriptEnd > 0) {
-          // Insert the plugins block after the 'buildscript' section
-          gradleFile =
-            gradleFile.slice(0, buildscriptEnd) +
-            pluginConfig +
-            gradleFile.slice(buildscriptEnd);
-          fs.writeFileSync(gradleFilePath, gradleFile, 'utf8');
-          console.log('Added custom plugins property after buildscript block');
-        } else {
-          console.error('Could not find the end of the buildscript block');
-        }
-      } else {
-        console.error('buildscript block not found in build.gradle');
-      }
-    } else {
-      console.log('Custom plugins block already exists in build.gradle');
-    }
-  } else {
-    console.error('build.gradle file not found');
-  }
+  gradleFile =
+    gradleFile.slice(0, buildscriptEnd) +
+    pluginConfig +
+    gradleFile.slice(buildscriptEnd);
+  fs.writeFileSync(gradleFilePath, gradleFile, 'utf8');
+  console.log('Added custom plugins property after buildscript block');
 };
